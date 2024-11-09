59d96df8d6cd53749051319145cd6855a0dae3c1
Masahiro OHTA <masahiro.ohta0211@gmail.com>
2024-11-09 21:50:11 +0000

    Add comments on draw

--- strategy_01/draw_open_gl.h
+++ strategy_01/draw_open_gl.h
@@ -6,6 +6,8 @@ class OpenGLStrategy : public DrawStragety {
 public:
     OpenGLStrategy() = default;
     virtual ~OpenGLStrategy() = default;
+
+    // draw needs to be modified if a new shape is added!
     void draw(const Circle& circle /*some arguments*/) const override;
     void draw(const Square& square /*some arguments*/) const override;
 };
